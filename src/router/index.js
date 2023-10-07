31f092b7a0167c5ab65a6178abec1586583fc129
Dev-BilalMunir <>
2023-10-07 16:01:41 +0500

    update router file

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,16 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 import Landing from '@/views/Landing.vue';
-// import ClientsView from '@/views/ClientsView.vue';
+
+const isGitHubPages = process.env.NODE_ENV === 'production';
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: isGitHubPages ? createWebHistory() : createWebHashHistory(),
   routes: [
     {
       path: '/',
       component: Landing,
-      // component: ClientsView,
-      // children: [
-      //   {
-      //     path: '/',
-      //     // component: () => import('@/views/HomeView.vue')
-      //     component: () => import('@/views/Landing.vue')
-      //   },
-      //   {
-      //     path: '/about',
-      //     name: 'about',
-      //     // route level code-splitting
-      //     // this generates a separate chunk (About.[hash].js) for this route
-      //     // which is lazy-loaded when the route is visited.
-      //     component: () => import('@/views/AboutView.vue')
-      //   },
-      //   {
-      //     path: '/landing',
-      //     name: 'landing',
-      //     // route level code-splitting
-      //     // this generates a separate chunk (About.[hash].js) for this route
-      //     // which is lazy-loaded when the route is visited.
-      //     component: () => import('@/views/Landing.vue')
-      //   }
-      // ]
-    }]
-    
-  });
+    },
+  ],
+});
 
 export default router;
