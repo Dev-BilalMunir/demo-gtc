e2dc11a010c0bb4a58ec27634ebe026b0e54f0d7
Dev-BilalMunir <>
2023-10-07 15:38:14 +0500

    added build folder

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,32 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+
 export default defineConfig({
   plugins: [
     vue(),
   ],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      },
+  base: '/demo-gtc/', // Update with your repository name
 })
+
+  
+
 
 
